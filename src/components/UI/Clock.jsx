d2b44540be89fc2ae4dcd0395b0d131ecd76541f
Ibrahimov Bekzod <107322765+Wave12345678@users.noreply.github.com>
2022-12-13 01:04:35 +0500

    Update Clock.jsx

--- src/components/UI/Clock.jsx
+++ src/components/UI/Clock.jsx
@@ -11,7 +11,7 @@ const Clock = () => {
     let interval;
 
     const countDown = () => {
-        const destination = new Date('Nov 10, 2022').getTime()
+        const destination = new Date('Nov 10, 2023').getTime()
 
         interval = setInterval(() => {
             const now = new Date().getTime()
